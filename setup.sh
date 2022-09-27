ccf7ebf73391eb29ddb8be2127d53bc715624ba5
Andrew Nissen <andrewnissen@Andrews-MBP.cable.rcn.com>
2022-09-27 18:58:36 -0400

    remove aws install, fix site upload command

--- setup.sh
+++ setup.sh
@@ -6,11 +6,6 @@ apt install imagemagick
 # install unzip
 apt install unzip
 
-# install aws-cli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
 echo "Make sure the full and thumbnail directories are readable by the public!"
 
 # configure the image processing cron job
